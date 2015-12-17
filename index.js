c1562eb5559c0971563ee6dc340792efa478437f
Per Gifvars <per@gifvars.com>
2015-12-17 21:40:30 +0000

    added reading of index.htlm

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
+var indexhtml;
+var fs = require("fs");
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2OB!')
-})
+      indexhtml = fs.readFileSync("index.html","utf8");
+      response.send(indexhtml);
+});
+
+
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});

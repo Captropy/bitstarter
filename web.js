62a86fb41bccbdcaf678049e231672ce01b702f5
Per Gifvars <per@gifvars.com>
2016-01-01 23:37:31 +0000

    Added stupid comment

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var indexhtml;
 var fs = require("fs");
 
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
